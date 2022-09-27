4630558d245a30320cb9a5d3069964a07bd4f7d9
Runzhi He <46741383+12f23eddde@users.noreply.github.com>
2022-09-28 01:20:35 +0800

    fix: diy.sh

--- diy.sh
+++ diy.sh
@@ -13,35 +13,35 @@
 echo $(cat .config | grep "=y")
 
 set_default(){
-    sed -i "s/#define $1 .*/#define $1 ${2@Q}/" trunk/user/shared/defaults.h
-    echo "Set Default $1:${2@Q}"
+    sed -i "s/#define $1 .*/#define $1 $2/" trunk/user/shared/defaults.h
+    echo "Set Default $1:$2"
 }
 
 #define DEF_LAN_ADDR		"192.168.2.1"
-set_default DEF_LAN_ADDR		"192.168.1.1"
+set_default DEF_LAN_ADDR		\"192.168.1.1\"
 #define DEF_LAN_DHCP_BEG	"192.168.2.100"
-set_default DEF_LAN_DHCP_BEG	"192.168.1.100"
+set_default DEF_LAN_DHCP_BEG	\"192.168.1.100\"
 #define DEF_LAN_DHCP_END	"192.168.2.244"
-set_default DEF_LAN_DHCP_END	"192.168.1.244"
+set_default DEF_LAN_DHCP_END	\"192.168.1.244\"
 #define DEF_LAN_MASK		"255.255.255.0"
-set_default DEF_LAN_MASK		"255.255.255.0"
+set_default DEF_LAN_MASK		\"255.255.255.0\"
 
 #define DEF_WLAN_2G_CC		"CN"
-set_default DEF_WLAN_2G_CC		"AU"
+set_default DEF_WLAN_2G_CC		\"AU\"
 #define DEF_WLAN_5G_CC		"US"
-set_default DEF_WLAN_5G_CC		"AU"
+set_default DEF_WLAN_5G_CC		\"AU\"
 #define DEF_WLAN_2G_PSK		"1234567890"
-set_default DEF_WLAN_2G_PSK		"1234567890"
+set_default DEF_WLAN_2G_PSK		\"1234567890\"
 #define DEF_WLAN_5G_PSK		"1234567890"
-set_default DEF_WLAN_5G_PSK		"1234567890"
+set_default DEF_WLAN_5G_PSK		\"1234567890\"
 
 #define DEF_ROOT_PASSWORD	"admin"
-set_default DEF_ROOT_PASSWORD	"admin"
+set_default DEF_ROOT_PASSWORD	\"admin\"
 #define DEF_SMB_WORKGROUP	"WORKGROUP"
-set_default DEF_SMB_WORKGROUP	"WORKGROUP"
+set_default DEF_SMB_WORKGROUP	\"WORKGROUP\"
 #define DEF_TIMEZONE		"CST-8"
-set_default DEF_TIMEZONE		"CST-8"
+set_default DEF_TIMEZONE		\"CST-8\"
 #define DEF_NTP_SERVER0		"ntp1.aliyun.com"
-set_default DEF_NTP_SERVER0		"ntp1.aliyun.com"
+set_default DEF_NTP_SERVER0		\"ntp1.aliyun.com\"
 #define DEF_NTP_SERVER1		"time1.cloud.tencent.com"
-set_default DEF_NTP_SERVER1		"time1.cloud.tencent.com"
+set_default DEF_NTP_SERVER1		\"time1.cloud.tencent.com\"
